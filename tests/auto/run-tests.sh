876fc2db5a51399a967fe1cac2639d0baae4232d
Marcin Albrecht <marcin.albrecht@oss.tieto.com>
2013-10-15 12:55:49 +0200

    Fix tests scripts to be runable

--- tests/auto/run-tests.sh
+++ tests/auto/run-tests.sh
@@ -2,9 +2,9 @@
 
 # Create a temporary DBus session to isolate us from the normal environment.
 export `dbus-launch`
-export QML_IMPORT_PATH=$PWD/../imports
+export QML2_IMPORT_PATH=/opt/tests/jolla-gallery/imports
 
-qmltestrunner $@
+/usr/lib/qt5/bin/qmltestrunner $@
 exit_code=$?
 
 kill $DBUS_SESSION_BUS_PID
